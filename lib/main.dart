43be1d549bc034d8e35ca7bfd2fa713a12084d89
Lovingcats <whehdrms6810@naver.com>
2024-01-03 12:58:40 +0900

    [ADD] calculator_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:neumorphism_calculator/provider/calculator_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +12,42 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ScreenUtilInit(
-        designSize: const Size(360, 800),
-        builder: (BuildContext context, Widget? child) => const MaterialApp(
-              title: 'StudyingTimer',
-              supportedLocales: [Locale('ko', 'KR')],
-              locale: Locale('ko'),
-              debugShowCheckedModeBanner: false,
-              home: Calculator(),
-            ));
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider<CalculatorProvider>(
+          create: (_) => CalculatorProvider(),
+        ),
+      ],
+      child: ScreenUtilInit(
+          designSize: const Size(360, 800),
+          builder: (BuildContext context, Widget? child) => const MaterialApp(
+                title: 'Neumorphism_Calculator',
+                supportedLocales: [Locale('ko', 'KR')],
+                locale: Locale('ko'),
+                debugShowCheckedModeBanner: false,
+                home: CalculatorLendering(),
+              )),
+    );
+  }
+}
+
+class CalculatorLendering extends StatefulWidget {
+  const CalculatorLendering({super.key});
+
+  @override
+  State<CalculatorLendering> createState() => _CalculaotrState();
+}
+
+class _CalculaotrState extends State<CalculatorLendering> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: null,
+      body: Center(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
