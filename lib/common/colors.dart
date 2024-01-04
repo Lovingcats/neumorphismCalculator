27359180c273e16af976bca8bad1e4483927b5ca
Lovingcats <whehdrms6810@naver.com>
2024-01-04 22:59:12 +0900

    [ADD] blackBackgroundColor, whiteBackgroundColor 추가

--- lib/common/colors.dart
+++ lib/common/colors.dart
@@ -3,11 +3,10 @@ import 'package:flutter/material.dart';
 class CommonColor {
   static Color beginCalculatorColor = const Color.fromARGB(255, 50, 216, 245);
   static Color endCalculatorColor = const Color.fromARGB(255, 21, 190, 196);
-  static Gradient textGradient = LinearGradient(colors: [
-                  beginCalculatorColor,
-                  endCalculatorColor
-                ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter
-              );
+  static Gradient textGradient = LinearGradient(
+      colors: [beginCalculatorColor, endCalculatorColor],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter);
+  static Color blackBackgroundColor = const Color(0xff393d45);
+  static Color whiteBackgroundColor = Colors.white;
 }
