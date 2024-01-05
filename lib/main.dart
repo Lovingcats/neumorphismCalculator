1e8c1b6c4e1b36369db62772e6994163cf2bee69
choyongje <whehdrms68101@gmail.com>
2024-01-05 13:11:50 +0900

    [Add] 다크모드 day_night_switcher 적용후 다크모드 수정

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:neumorphism_calculator/common/colors.dart';
 import 'package:neumorphism_calculator/provider/calculator_provider.dart';
 import 'package:neumorphism_calculator/widget/gradint_text.dart';
 import 'package:provider/provider.dart';
+import 'package:day_night_switcher/day_night_switcher.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,8 +28,7 @@ class MyApp extends StatelessWidget {
                 title: 'Neumorphism_Calculator',
                 debugShowCheckedModeBanner: false,
                 theme: NeumorphicThemeData(
-                  baseColor:
-                      Color.fromARGB(255, 242, 242, 242).withOpacity(0.9),
+                  baseColor: CommonColor.whiteBackgroundColor,
                   lightSource: LightSource.topLeft,
                   depth: 10,
                 ),
@@ -55,37 +55,56 @@ class _CalculaotrState extends State<CalculatorLendering> {
   Widget build(BuildContext context) {
     var calculatorProvider = Provider.of<CalculatorProvider>(context);
     return Scaffold(
-      appBar: null,
       backgroundColor: NeumorphicTheme.baseColor(context),
+      appBar: AppBar(
+        leadingWidth: 78.w,
+        backgroundColor: NeumorphicTheme.of(context)!.isUsingDark ? CommonColor.blackBackgroundColor : CommonColor.whiteBackgroundColor,
+        leading: Padding(
+          padding: EdgeInsets.only(
+            left: 10.w
+          ),
+          child: InkWell(
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.transparent,
+                        hoverColor: Colors.transparent,
+                        onTap: (){
+                          calculatorProvider.changeTheme(context);
+                          calculatorProvider.changeTextColor(context);
+                          calculatorProvider.changeIconColor(context);
+                        },
+                        child: AbsorbPointer(
+                          child: SizedBox(
+                            child: DayNightSwitcher(
+                                  isDarkModeEnabled: NeumorphicTheme.of(context)!.isUsingDark,
+                                  onStateChanged: (isDarkModeEnabled) {
+                                  },
+                            ),
+                          ),
+                        ),
+                      ),
+        ),
+      ),
       body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Padding(
-                padding: EdgeInsets.only(left: 10.w, right: 10.w),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    NeumorphicButton(
-                      onPressed: () {
-                        calculatorProvider.changeTheme(context);
-                        calculatorProvider.changeTextColor(context);
-                        calculatorProvider.changeIconColor(context);
-                      },
-                      style: const NeumorphicStyle(
-                        shape: NeumorphicShape.concave,
-                        boxShape: NeumorphicBoxShape.circle(),
-                      ),
-                      padding: EdgeInsets.all(12.w),
-                      child: Icon(
-                        Icons.favorite_border,
-                        color: calculatorProvider.iconcolor,
-                      ),
-                    ),
-                  ],
-                ),
-              )
+                    // NeumorphicButton(
+                    //   onPressed: () {
+                    //     calculatorProvider.changeTheme(context);
+                    //     calculatorProvider.changeTextColor(context);
+                    //     calculatorProvider.changeIconColor(context);
+                    //   },
+                    //   style: const NeumorphicStyle(
+                    //     shape: NeumorphicShape.concave,
+                    //     boxShape: NeumorphicBoxShape.circle(),
+                    //   ),
+                    //   padding: EdgeInsets.all(12.w),
+                    //   child: Icon(
+                    //     Icons.favorite_border,
+                    //     color: calculatorProvider.iconcolor,
+                    //   ),
+                    // ),
             ],
           ),
         ),
