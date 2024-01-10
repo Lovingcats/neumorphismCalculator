d536347cc0a11b55441636f30fc5c1029da81ece
Lovingcats <whehdrms6810@naver.com>
2024-01-10 21:59:36 +0900

    [ADD] cCalculateText 위젯 분리완료

--- lib/widget/calculator_widget.dart
+++ lib/widget/calculator_widget.dart
@@ -4,6 +4,7 @@ import 'package:flutter_neumorphic_plus/flutter_neumorphic.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:neumorphism_calculator/common/colors.dart';
 import 'package:neumorphism_calculator/provider/calculator_provider.dart';
+import 'package:neumorphism_calculator/widget/gradint_text.dart';
 
 AppBar cAppbar(BuildContext context, CalculatorProvider calculatorProvider) {
   return AppBar(
@@ -31,6 +32,34 @@ AppBar cAppbar(BuildContext context, CalculatorProvider calculatorProvider) {
           ),
         ),
       ),
+    ),
+  );
+}
+
+Padding cCalculateText(CalculatorProvider calculatorProvider) {
+  return Padding(
+    padding: EdgeInsets.only(top: 85.h, right: 15.w),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: [
+            GradientText(
+              "2,020 x 2 - 1,942",
+              gradient: CommonColor.textGradient,
+              style: TextStyle(fontSize: 13.sp),
+            ),
+            Text(
+              "2,098",
+              style: TextStyle(
+                  fontSize: 50.sp,
+                  color: calculatorProvider.textcolor,
+                  fontWeight: FontWeight.w400),
+            )
+          ],
+        )
+      ],
     ),
   );
 }
